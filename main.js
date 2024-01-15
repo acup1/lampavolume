c2dcb203753a4c87b653efe6b4f5ea28ad3994c5
Egor Poroshin <georgiyegoriy@gmail.com>
2024-01-15 23:53:16 +0300

    fix

--- main.js
+++ main.js
@@ -19,37 +19,9 @@ onkeydown = (event) => {
     el='<input class="volumeinput selector" type="range" min="0" max="1" step="0.05" value="0.3" style="margin-left: 50px;" oninput="changevolume();"></input>';
 
 
-    vdbtn=```
-        <div class="button selector volumedown" data-controller="player_panel">
-            <div>
-                <svg width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M6 12L18 12" stroke="currentcolor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
-                </svg>
-            </div>
-            <div>
-                <svg width="19" height="25" viewBox="0 0 19 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <rect width="6" height="25" rx="2" fill="currentColor"></rect>
-                <rect x="13" width="6" height="25" rx="2" fill="currentColor"></rect>
-                </svg>                    
-            </div>
-        </div>
-    ```;
+    vdbtn='<div class="button selector volumedown" data-controller="player_panel"><div><svg width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6 12L18 12" stroke="currentcolor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg></div><div><svg width="19" height="25" viewBox="0 0 19 25" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="6" height="25" rx="2" fill="currentColor"></rect><rect x="13" width="6" height="25" rx="2" fill="currentColor"></rect></svg></div></div>';
 
-    vubtn=```
-        <div class="button selector volumeup" data-controller="player_panel">
-            <div>
-            <svg width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M4 12H20M12 4V20" stroke="currentcolor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                </svg>
-            </div>
-            <div>
-                <svg width="19" height="25" viewBox="0 0 19 25" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <rect width="6" height="25" rx="2" fill="currentColor"></rect>
-                <rect x="13" width="6" height="25" rx="2" fill="currentColor"></rect>
-                </svg>                    
-            </div>
-        </div>
-    ```;
+    vubtn='<div class="button selector volumeup" data-controller="player_panel"><div><svg width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M4 12H20M12 4V20" stroke="currentcolor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg></div><div><svg width="19" height="25" viewBox="0 0 19 25" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="6" height="25" rx="2" fill="currentColor"></rect><rect x="13" width="6" height="25" rx="2" fill="currentColor"></rect></svg></div></div>';
 
     observer = new MutationObserver(function(mutations) {
         mutations.forEach(function(mutation) {
