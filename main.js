5cc4d6ae36982a7811ee7384ddf282553646b0b7
Egor Poroshin <georgiyegoriy@gmail.com>
2024-01-15 06:34:15 +0300

    123

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-(function () {
-setTimeout(() => {
-    $(".head__logo-icon")[0].children[0].src="";
-},2000);
+(() => {
+    $(".player-panel__left")[0].innerHTML=$(".player-panel__left")[0].innerHTML+'<input type="range" min="0" max="1" step="0.05" value="0.3"></input>'
 })(); 
