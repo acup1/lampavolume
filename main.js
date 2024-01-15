e58151b0f1fcfd912599febaf0e2a6e86eac52a8
Egor Poroshin <georgiyegoriy@gmail.com>
2024-01-15 06:43:03 +0300

    1

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 (() => {
-    $(".player-panel__left")[0].innerHTML=$(".player-panel__left")[0].innerHTML+'<input type="range" min="0" max="1" step="0.05" value="0.3"></input>'
+    el='<input class="volumeinput" type="range" min="0" max="1" step="0.05" value="0.3"></input>'
+
+    observer = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+            if ($(".player-panel__left")[0]!=undefined && $(".volumeinput")[0]==undefined){
+                $(".player-panel__left")[0].innerHTML=$(".player-panel__left")[0].innerHTML+el
+            }
+        });    
+      });
+
+    observer.observe(Document,  {attributes:true, childList:true, characterData:true});
 })(); 
